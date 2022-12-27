f050e4e675c5462f7976013478715832a020f489
hdn <hieudn5293@yahoo.de>
2022-12-27 19:11:55 +0100

    feat(events): add creation data

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,5 +1,8 @@
+import { autoinject } from "aurelia-framework";
+import { WalletService } from "services/WalletService";
 import "./events.scss";
 
+@autoinject
 export class Events {
   isCreateEvent = false;
 
@@ -8,6 +11,8 @@ export class Events {
   private newEventTime: string;
   private newEventUrl: string;
   private newEventAttendees: string;
+
+  constructor(private walletService: WalletService) {}
 
   private async enableCreateEvent() {
     this.isCreateEvent = true;
